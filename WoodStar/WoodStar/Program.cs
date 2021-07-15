a531e4d755c6473227e70f60807e69148022fb05
Smit Patel <smitpatel@users.noreply.github.com>
2021-07-15 13:42:59 -0700

    stub

---
+++ WoodStar/WoodStar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WoodStar
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new WoodStarConnection("10.130.64.58", "sa", "Password1!", "master");
+            await connection.OpenAsync();
+
+            Console.WriteLine("Success");
+        }
+    }
+}
---
+++ WoodStar/WoodStar/TdsHeader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Buffers;
+
+namespace WoodStar
+{
+    public class TdsHeader
+    {
+        public static int HeaderSize = 8;
+
+        public TdsHeader(PacketType packetType, PacketStatus packetStatus, int length, int spid, int packetId)
+        {
+            Type = packetType;
+            Status = packetStatus;
+            Length = length;
+            SPID = spid;
+            PacketId = packetId;
+        }
+
+        public PacketType Type { get; }
+        public PacketStatus Status { get; }
+        public int Length { get; } // 2-byte
+        public int SPID { get; } // 2-byte
+        public int PacketId { get; } // 1-byte
+        public byte Window => 0;
+
+        public void WriteToBuffer(byte[] buffer)
+        {
+            var i = 0;
+            buffer[i++] = (byte)Type;
+            buffer[i++] = (byte)Status;
+            buffer[i++] = (byte)(Length >> 8);
+            buffer[i++] = (byte)Length;
+            buffer[i++] = (byte)(SPID >> 8);
+            buffer[i++] = (byte)SPID;
+            buffer[i++] = (byte)PacketId;
+            buffer[i++] = Window;
+        }
+
+        public static TdsHeader Parse(ReadOnlySequence<byte> buffer)
+        {
+            // TODO: Use sequence reader.
+            if (buffer.Length != 8
+                || !buffer.IsSingleSegment)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var span = buffer.FirstSpan;
+            return new TdsHeader(
+                (PacketType)span[0], (PacketStatus)span[1], ConvertBytes(span[2], span[3]), ConvertBytes(span[4], span[5]), span[6]);
+        }
+
+        private static int ConvertBytes(byte h, byte l)
+        {
+            return h * 0xFF + l;
+        }
+    }
+}
